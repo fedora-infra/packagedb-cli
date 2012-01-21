a162668e41a76851a524d313481d0194ad07a3d1
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-01-21 17:11:46 +0100

    Add a setup.py script

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Setup script
+"""
+
+from distutils.core import setup
+
+setup(
+    name = 'packagedb-cli',
+    description = 'A command line tool to access the Fedora Package Database.',
+    version = '1.2.1',
+    license = 'GPLv2+',
+    download_url = 'https://fedorahosted.org/releases/p/a/packagedb-cli/',
+    url = 'https://fedorahosted.org/packagedb-cli/',
+    scripts=['pkgdb-cli'],
+    )
