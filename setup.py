87a6bb5bcbf92867c2f0b95a61dc33a811e3a860
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-01-10 22:57:11 +0100

    Prepare release 1.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 setup(
     name = 'packagedb-cli',
     description = 'A command line tool to access the Fedora Package Database.',
-    version = '1.2.1',
+    version = '1.3.0',
     license = 'GPLv2+',
     download_url = 'https://fedorahosted.org/releases/p/a/packagedb-cli/',
     url = 'https://fedorahosted.org/packagedb-cli/',
