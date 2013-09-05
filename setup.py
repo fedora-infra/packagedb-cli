1d17a093b9e0328db2777f48db2c9498aa78234a
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-09-05 12:04:24 +0200

    Bump release and remove some trailing space, replace some tabs by spaces

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ from distutils.core import setup
 setup(
     name = 'packagedb-cli',
     description = 'A command line tool to access the Fedora Package Database.',
-    version = '1.5.1',
+    version = '1.6.0',
     license = 'GPLv2+',
     download_url = 'https://fedorahosted.org/releases/p/a/packagedb-cli/',
     url = 'https://fedorahosted.org/packagedb-cli/',
     scripts=['pkgdb-cli'],
-	author='Pierre-Yves Chibon',
-	author_email='pingou@pingoured.fr',
+    author='Pierre-Yves Chibon',
+    author_email='pingou@pingoured.fr',
     )
