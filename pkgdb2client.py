68f9eba9e8b6e349e728653f8a82c18ffdd48ee1
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-05-14 17:44:31 +0200

    Fix the default url to point to the prod instance of pkgdb2

--- pkgdb2client.py
+++ pkgdb2client.py
@@ -24,7 +24,7 @@ logging.basicConfig()
 LOG = logging.getLogger("pkgdb2client")
 
 __version__ = '2.0'
-PKGDB_URL = r'https://admin.stg.fedoraproject.org/pkgdb/'
+PKGDB_URL = r'https://admin.fedoraproject.org/pkgdb/'
 
 
 class PkgDBException(Exception):
