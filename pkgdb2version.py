5a4cdbe0f663d01c460207ed7b3b17036c0efab2
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-05-20 11:40:16 +0200

    Add a dedicated pkgdb2version file that contains only the version information

---
+++ pkgdb2version.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+# pkgdb2 - a commandline frontend for the Fedora package database v2
+#
+# Copyright (C) 2014 Red Hat Inc
+# Copyright (C) 2013 Pierre-Yves Chibon
+# Author: Pierre-Yves Chibon <pingou@pingoured.fr>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+# See http://www.gnu.org/copyleft/gpl.html  for the full text of the
+# license.
+"""
+
+__version__ = '2.2'
